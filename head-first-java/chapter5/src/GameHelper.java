adcd2ba63e1b9c58b2a15e10dba6aef06db9149e
Vayne-Lover <406378362@qq.com>
2017-02-22 19:01:21 +0800

    Use Scanner class to read input.

--- head-first-java/chapter5/src/GameHelper.java
+++ head-first-java/chapter5/src/GameHelper.java
@@ -2,17 +2,21 @@
  * Created by Vayne-Lover on 2/22/17.
  */
 import java.io.*;
+import java.util.Scanner;
+
 public class GameHelper {
     public String GetUserInput(){
         String input=null;
         System.out.print("Please input your guess : ");
         try{
-            BufferedReader is=new BufferedReader(new InputStreamReader(System.in));
-            input=is.readLine();
+            //BufferedReader is=new BufferedReader(new InputStreamReader(System.in));
+            //input=is.readLine();
+            Scanner sc=new Scanner(System.in);
+            input=sc.nextLine();
             if(input.length()==0){
                 return null;
             }
-        }catch(IOException e){
+        }catch(Exception e){
             System.out.println(e);
         }
         return input;
