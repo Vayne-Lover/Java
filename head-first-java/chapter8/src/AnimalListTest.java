2f05985319f3381f9f023626317677f86117f58f
Vayne-Lover <406378362@qq.com>
2017-05-09 13:22:49 +0800

    Add test.

---
+++ head-first-java/chapter8/src/AnimalListTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Vayne-Lover on 5/9/17.
+ */
+public class AnimalListTest {
+    public static void main(String[] args){
+        AnimalList al = new AnimalList();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        al.add(dog);
+        al.add(cat);
+    }
+}
