7883ae7de47b900dfe52bdc2dcaa3ee7226f24c5
Vayne-Lover <406378362@qq.com>
2017-05-10 14:27:22 +0800

    Add child class and test.

---
+++ head-first-java/chapter9/src/Hippo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Vayne-Lover on 5/10/17.
+ */
+public class Hippo extends Animal {
+    public Hippo(String name){
+        super(name);
+    }
+}
---
+++ head-first-java/chapter9/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Vayne-Lover on 5/10/17.
+ */
+public class Test {
+    public static void main(String[] args){
+       Hippo hip = new Hippo("HH");
+       System.out.println(hip.getName());
+    }
+}
