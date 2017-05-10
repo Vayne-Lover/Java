326bc881ba7019260202e1393ee6631d0acfdc16
Vayne-Lover <406378362@qq.com>
2017-05-10 14:19:18 +0800

    Add abstract class Animal.

---
+++ head-first-java/chapter9/src/Animal.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Vayne-Lover on 5/10/17.
+ */
+abstract public class Animal {
+    private String name;
+
+    public Animal(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
