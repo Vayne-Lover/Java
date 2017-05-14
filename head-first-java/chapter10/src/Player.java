dfd83639285a9433a531b2a37a294f100c4f5113
Vayne-Lover <406378362@qq.com>
2017-05-14 16:15:45 +0800

    Add class Player.

---
+++ head-first-java/chapter10/src/Player.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Vayne-Lover on 5/14/17.
+ */
+public class Player {
+    static int count = 0;
+    private String name;
+
+    public Player(String n){
+        name = n;
+        count++;
+    }
+}
