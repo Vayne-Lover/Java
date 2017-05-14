d1e713ba02cfa1b0ea3f821c1c0f45851cba2054
Vayne-Lover <406378362@qq.com>
2017-05-14 16:18:10 +0800

    Add test.

---
+++ head-first-java/chapter10/src/PlayerTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Vayne-Lover on 5/14/17.
+ */
+public class PlayerTest {
+    public static void main(String[] args){
+        Player player1 = new Player("Ply1");
+        System.out.println(Player.count);
+        Player player2 = new Player("Ply2");
+        System.out.println(Player.count);
+    }
+}
