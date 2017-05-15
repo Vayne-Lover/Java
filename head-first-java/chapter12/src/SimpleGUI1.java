4d4b8c63570c546f3aec82883622d2b174c5a791
Vayne-Lover <406378362@qq.com>
2017-05-15 14:29:44 +0800

    Add class SimpleGUI1.

---
+++ head-first-java/chapter12/src/SimpleGUI1.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Vayne-Lover on 5/15/17.
+ */
+import javax.swing.*;
+
+public class SimpleGUI1 {
+    public static void main(String[] argv){
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click me!");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(button);
+
+        frame.setSize(200,200);
+
+        frame.setVisible(true);
+
+    }
+}
