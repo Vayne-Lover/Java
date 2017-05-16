ae0a0f65544d5fbb3acbac62eb63be00134ec5a1
Vayne-Lover <406378362@qq.com>
2017-05-16 14:09:56 +0800

    Add class SimpleGUI3.

--- head-first-java/chapter12/src/SimpleGUI3.java
+++ head-first-java/chapter12/src/SimpleGUI3.java
@@ -1,3 +1,24 @@
 /**
  * Created by Vayne-Lover on 5/16/17.
  */
+import java.awt.*;
+import javax.swing.*;
+
+public class SimpleGUI3 {
+    JFrame frame;
+
+    public static void main(String[] args){
+        SimpleGUI3 gui = new SimpleGUI3();
+        gui.go();
+    }
+
+    public void go(){
+        frame = new JFrame();
+        DrawPanel panel = new DrawPanel();
+
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+}
