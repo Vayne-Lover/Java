6db875174b1f11c93684b07f49769e8b7d213861
Vayne-Lover <406378362@qq.com>
2017-05-27 13:52:33 +0800

    Add chapter 15.

---
+++ head-first-java/chapter15/src/Client.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Vayne-Lover on 5/27/17.
+ */
+public class Client {
+    public void go(){
+
+    }
+    public static void main(String[] args){
+        Client cli = new Client();
+        cli.go();
+    }
+
+}
+
