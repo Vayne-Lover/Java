708c2c31e3f6ab1eb74bc591535be8d9a6fe7904
Vayne-Lover <406378362@qq.com>
2017-05-27 13:58:39 +0800

    Finish class Client.

--- head-first-java/chapter15/src/Client.java
+++ head-first-java/chapter15/src/Client.java
@@ -1,9 +1,29 @@
 /**
  * Created by Vayne-Lover on 5/27/17.
  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
     public void go(){
+        try{
+            Socket s = new Socket("127.0.0.1", 4242);
 
+            InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+
+            String advice = reader.readLine();
+
+            System.out.println(advice);
+
+            reader.close();
+
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
     public static void main(String[] args){
         Client cli = new Client();
