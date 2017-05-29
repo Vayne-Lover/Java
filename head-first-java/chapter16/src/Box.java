15d1a29513757829d9572d8f43ae3958987279f3
Vayne-Lover <406378362@qq.com>
2017-05-29 13:24:44 +0800

    Add chapter 16 and class Box.

---
+++ head-first-java/chapter16/src/Box.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Vayne-Lover on 5/29/17.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Box {
+    private ArrayList<String> list = new ArrayList<String>();
+
+    public static void main(String[] args){
+        Box box = new Box();
+        box.go();
+    }
+
+    private void go(){
+        getSongs();
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+
+    private void getSongs(){
+        try{
+            File file = new File("Songs.txt");
+            BufferedReader reader = new BufferedReader(file);
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                addSong(line);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void addSong(String line){
+        String[] songs = line.split("/");
+        list.add(songs[0]);
+    }
+}
