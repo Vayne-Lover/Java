f6a6c9c8901d038b1c179b1fafa7ae4ed36f8ea5
Vayne-Lover <406378362@qq.com>
2017-06-02 10:06:00 +0800

    Add abstract class Animal and two child class.

---
+++ head-first-java/chapter16/src/TestAnimal.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Vayne-Lover on 6/2/17.
+ */
+abstract class Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){}
+}
+
+class Cat extends Animal{
+    void meow(){}
+}
+
+public class TestAnimal {
+    
+}
