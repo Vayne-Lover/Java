d49ff1eb959393d2b93b94cd69d1785ccdbc387d
Vayne-Lover <406378362@qq.com>
2017-06-02 10:19:33 +0800

    Add test.

--- head-first-java/chapter16/src/TestAnimal.java
+++ head-first-java/chapter16/src/TestAnimal.java
@@ -1,20 +1,39 @@
+import java.util.ArrayList;
+
 /**
  * Created by Vayne-Lover on 6/2/17.
  */
+
 abstract class Animal{
     void eat(){
-        System.out.println("Eating");
+        System.out.println("Animal eating");
     }
 }
 
 class Dog extends Animal{
-    void bark(){}
+    void eat(){
+        System.out.println("Dog eating");
+    }
 }
 
 class Cat extends Animal{
-    void meow(){}
+    void eat(){
+        System.out.println("Cat eating");
+    }
 }
 
 public class TestAnimal {
-    
+    public static void take(ArrayList<? extends Animal> list){
+        for(Animal a:list){
+            a.eat();
+        }
+    }
+    public static void main(String[] args){
+        ArrayList<Animal> animals = new ArrayList<Animal>();
+
+        animals.add(new Dog());
+        animals.add(new Cat());
+
+        take(animals);
+    }
 }
