64383f9638a39917d2db0d9b5e289d866e8abc3c
Vayne-Lover <406378362@qq.com>
2017-06-12 14:28:17 +0800

    Fix bugs and test it.

--- thinking-in-java/chapter13/TestRegex.java
+++ thinking-in-java/chapter13/TestRegex.java
@@ -2,8 +2,11 @@ import java.util.regex.*;
 
 public class TestRegex{
     public static void main(String[] args){
+        System.out.println(args);
+
         if (args.length < 2){
-            System.out.println("Error");
+            System.out.println("Error!");
+            System.out.println("Usage: abcabcd abc+");
             System.exit(0);
         }
 
@@ -12,7 +15,8 @@ public class TestRegex{
             Matcher m = p.matcher(args[0]);
 
             while(m.find()){
-                System.out.println("Match \""+m.group()+"\" at " + m.start() + "-" + m.end() - 1);
+                System.out.println("Regex:" + arg);
+                System.out.println("Match \""+m.group()+"\" at " + m.start() + "-" + (m.end() - 1));
             }
         }
     }
