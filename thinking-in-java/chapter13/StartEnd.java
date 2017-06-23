4f24eaacd54b4fed80a925f76c0719c865b138e2
Vayne-Lover <406378362@qq.com>
2017-06-23 19:16:13 +0800

    Add class StartEnd and static String input.

---
+++ thinking-in-java/chapter13/StartEnd.java
@@ -0,0 +1,9 @@
+import java.util.regex.*;
+
+public class StartEnd{
+    pubic static String input = "As long as there is injustuce, whenever a\n" +
+                                "Targeathian baby cries out, whenervr a distress\n" +
+                                "signal sounds among the stars ... We'l be there.\n" +
+                                "This fine ship, and this fine crew ...\n" +
+                                "Never give up!Never surrender!";
+}
