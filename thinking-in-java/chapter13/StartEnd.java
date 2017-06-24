4faa1f504e10682ad79a9fcf18e8798c22be83c9
Vayne-Lover <406378362@qq.com>
2017-06-24 15:59:18 +0800

    Add private class Display.

--- thinking-in-java/chapter13/StartEnd.java
+++ thinking-in-java/chapter13/StartEnd.java
@@ -6,4 +6,17 @@ public class StartEnd{
                                 "signal sounds among the stars ... We'l be there.\n" +
                                 "This fine ship, and this fine crew ...\n" +
                                 "Never give up!Never surrender!";
+
+    private class Display{
+        private boolean regexPrinted = false;
+        private String regex;
+        Display(String regex){this.regex = regex;}
+        void display(String message){
+            if(!regexPrinted){
+                System.out.println(regex);
+                regexPrinted = true;
+            }
+            System.out.println(message);
+        }
+    }
 }
