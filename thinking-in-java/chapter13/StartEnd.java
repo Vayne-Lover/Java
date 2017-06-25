92fd8be2091150fb134c67aae08ce7b89e1459b3
Vayne-Lover <406378362@qq.com>
2017-06-25 15:09:33 +0800

    Add function examine.

--- thinking-in-java/chapter13/StartEnd.java
+++ thinking-in-java/chapter13/StartEnd.java
@@ -19,4 +19,22 @@ public class StartEnd{
             System.out.println(message);
         }
     }
+
+    public static void examine(String s, String regex){
+        Display d = new Display(regex);
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(s);
+
+        while(m.find()){
+            d.display("find() ' " + m.group() + " ' start = " + m.start() + "end = " + m.end());
+
+            if(m.lookingAt()){
+                d.display("lookingAt() " + " start = " + m.start() + "end = " + m.end());
+            }
+
+            if(m.matches()){
+                d.display("matches() " + " start = " + m.start() + "end = " + m.end());
+            }
+        }
+    }
 }
