4e9bcc05cb5c34a54876bdc6128d6b109ae0c450
Vayne-Lover <406378362@qq.com>
2017-07-01 23:48:04 +0800

    Add class PrimitiveOverloading.

---
+++ thinking-in-java/chapter05/PrimitiveOverloading.java
@@ -0,0 +1,24 @@
+public class PrimitiveOverloading{
+    void f1(char x){System.out.println("fi(char)");}
+    void f1(byte x){System.out.println("fi(byte)");}
+    void f1(short x){System.out.println("fi(short)");}
+    void f1(int x){System.out.println("fi(int)");}
+    void f1(long x){System.out.println("fi(long)");}
+    void f1(float x){System.out.println("fi(float)");}
+
+    public void testChar(){
+        char x = 'x';
+        f1(x);
+    }
+
+    public void testByte(){
+        byte x = 0;
+        f1(x);
+    }
+
+    public static void main(String[] args){
+        PrimitiveOverloading p = new PrimitiveOverloading();
+        p.testChar();
+        p.testByte();
+    }
+}
