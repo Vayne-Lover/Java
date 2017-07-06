ff8c05d93843411764045b4b23e4a6ec340a3cce
Vayne-Lover <406378362@qq.com>
2017-07-07 00:25:44 +0800

    Add class BananaPeel.

---
+++ thinking-in-java/chapter05/BananaPeel.java
@@ -0,0 +1,10 @@
+public class BananaPeel{
+    public void peel(int i){}
+
+    public static void main(String[] args){
+        BananaPeel a = new BananaPeel();
+        BananaPeel b = new BananaPeel();
+        a.peel(2);
+        b.peel(3);
+    }
+}
